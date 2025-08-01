89cc6f2f62f7be36eb8301c3c6cb052bb3a05e88
alifpdf <alif.rafayel@hotmail.fr>
2025-08-01 10:19:05 +0200

    Final

--- src/main/java/finalagent/Main.java
+++ src/main/java/finalagent/Main.java
@@ -9,41 +9,41 @@ import jade.wrapper.ContainerController;
 public class Main {
 
     public static void main(String[] args) {
-        
-        
-      
-        
         ContainerController cc = null;
         Runtime rt = Runtime.instance();
 
         try {
-     
-            
-            
-
-            if (args.length<4) {
+            // If fewer than 4 arguments are provided, this is the main container
+            if (args.length < 4) {
                 Profile p = new ProfileImpl("localhost", 60000, null);
-                p.setParameter(Profile.GUI, "true");
-                p.setParameter(Profile.LOCAL_PORT, "60000");
-                
+                p.setParameter(Profile.GUI, "true"); // Enable the JADE GUI
+                p.setParameter(Profile.LOCAL_PORT, "60000"); // Set the default port
                 p.setParameter(Profile.MTPS, "jade.mtp.http.MessageTransportProtocol(http://0.0.0.0:8888/acc)");
-               cc = rt.createMainContainer(p);
+                cc = rt.createMainContainer(p); // Create the main container
             } else {
+                // If 4 arguments or more, create a secondary container and connect to the remote host
                 Profile p = new ProfileImpl(args[3], 60000, null);
                 p.setParameter(Profile.MTPS, "jade.mtp.http.MessageTransportProtocol(http://0.0.0.0:8888/acc)");
-                cc = rt.createAgentContainer(p);
+                cc = rt.createAgentContainer(p); // Join the existing platform
                 System.out.println("Starting as a secondary container");
             }
 
-            Object[] agentArgs = { args.length < 4, Integer.parseInt(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2])  };            
-             AgentController ac = cc.createNewAgent("Z"+args[0], "finalagent.SensorAgent", agentArgs);
+            // Prepare arguments to pass to the agent:
+            // [isMainContainer, ID, x-coordinate, y-coordinate]
+            Object[] agentArgs = {
+                args.length < 4,
+                Integer.parseInt(args[0]),
+                Double.parseDouble(args[1]),
+                Double.parseDouble(args[2])
+            };
+
+            // Create and start the agent named Z{ID}
+            AgentController ac = cc.createNewAgent("Z" + args[0], "finalagent.SensorAgent", agentArgs);
             ac.start();
 
-         
         } catch (Exception e) {
+            // Print stack trace on error
             e.printStackTrace();
-
-
         }
     }
 }
